3bb7c54b0fb0605fe8b90e7fab53318f9f8bcde0
abhinav159 <abhinavsuresh159@gmail.com>
2022-12-25 14:39:23 +0530

    adding

---
+++ feb.py
@@ -0,0 +1,32 @@
+# n_terms = int(input ("How many terms the user wants to print? "))  
+# n_1 = 0
+# n_2 = 1
+# count = 0
+
+# print ("The fibonacci sequence of the numbers is:")  
+# while count < n_terms:  
+#         print(n_1)  
+#         nth = n_1 + n_2  
+#        # At last, we will update values  
+#         n_1 = n_2  
+#         n_2 = nth  
+#         count += 1  
+
+
+# num = int(input("Enter a number: "))
+
+# # initialize sum
+# sum = 0
+
+# # find the sum of the cube of each digit
+# temp = num         
+# while temp > 0:
+#    digit = temp % 10         
+#    sum += digit ** 3                     
+#    temp //= 10              
+
+# # display the result
+# if num == sum:
+#    print(num,"is an Armstrong number")
+# else:
+#    print(num,"is not an Armstrong number")
---
+++ function.py
@@ -0,0 +1,10 @@
+def sum(a,b):
+    
+    sum= a+b
+    sub=a-b
+    return sum,sub
+
+res1,res2=sum(4,5)
+print( 'sum',res1)
+print( 'sup',res2)
+
